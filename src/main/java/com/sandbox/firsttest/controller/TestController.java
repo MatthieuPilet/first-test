8b712fb05fad666cc0e8c9003693e87d321448b1
DESKTOP-M56A960\matth <matthieu.pilet.dev@gmail.com>
2023-01-09 22:58:40 +0100

    [ADD]H2 local in memory database

--- src/main/java/com/sandbox/firsttest/controller/TestController.java
+++ src/main/java/com/sandbox/firsttest/controller/TestController.java
@@ -1,10 +1,13 @@
-package com.sandbox.firsttest.controllers;
+package com.sandbox.firsttest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
+	
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
