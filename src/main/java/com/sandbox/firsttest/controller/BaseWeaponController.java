90ead2f5f0ae4ae2c44fa69934b72422c32fc7c8
DESKTOP-M56A960\matth <matthieu.pilet.dev@gmail.com>
2023-06-21 22:49:33 +0200

    [UPDATE] Remove unused import

--- src/main/java/com/sandbox/firsttest/controller/BaseWeaponController.java
+++ src/main/java/com/sandbox/firsttest/controller/BaseWeaponController.java
@@ -1,24 +1,15 @@
 package com.sandbox.firsttest.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sandbox.firsttest.dto.AIRequestDto;
-import com.sandbox.firsttest.dto.AIResponseDto;
 import com.sandbox.firsttest.dto.BWResponseDto;
-import com.sandbox.firsttest.dto.UpdateAIRequestDto;
 import com.sandbox.firsttest.services.impl.BaseWeaponServiceImpl;
 
 /**
--- src/main/java/com/sandbox/firsttest/dto/BWResponseDto.java
+++ src/main/java/com/sandbox/firsttest/dto/BWResponseDto.java
@@ -1,7 +1,6 @@
 package com.sandbox.firsttest.dto;
 
 import java.math.BigInteger;
-import java.util.Date;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
