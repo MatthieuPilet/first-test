389bcac8021bda49590dcf2debff875757c88aeb
DESKTOP-M56A960\matth <matthieu.pilet.dev@gmail.com>
2024-08-25 15:26:08 +0200

    [UPDATE] switch to postgreSQL instead of H2 for database

--- src/main/resources/db/migration/V1_01__createschema.sql
+++ src/main/resources/db/migration/V1_01__createschema.sql
@@ -1,10 +1,34 @@
+DROP TABLE IF EXISTS NOMENCLATURE;  
+DROP SEQUENCE IF EXISTS nomenclature_id_seq;
+
+DROP TABLE IF EXISTS CHARACTERS;  
+DROP SEQUENCE IF EXISTS characters_seq;
+
 DROP TABLE IF EXISTS ACCOUNT_INFORMATION;  
 DROP SEQUENCE IF EXISTS account_information_seq;
+
+DROP TABLE IF EXISTS BASE_WEAPON;  
+DROP SEQUENCE IF EXISTS base_weapon_seq;
+
+DROP TABLE IF EXISTS CHARACTERS_STATS;  
+DROP SEQUENCE IF EXISTS characters_stats_seq;
+
+DROP TABLE IF EXISTS SKILLS;  
+DROP SEQUENCE IF EXISTS skill_id_seq;
+
+CREATE SEQUENCE nomenclature_id_seq START WITH 1 INCREMENT BY 1;
+CREATE TABLE NOMENCLATURE
+(
+    nomenclature_id BIGINT  DEFAULT nextval('nomenclature_id_seq') PRIMARY KEY NOT NULL,
+    nomenclature_code VARCHAR(50),
+    nomenclature_category VARCHAR(50), 
+    nomenclature_label VARCHAR(255)
+);
 
 CREATE SEQUENCE account_information_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE ACCOUNT_INFORMATION
 (
-	account_id BIGINT DEFAULT NEXT VALUE FOR account_information_seq PRIMARY KEY NOT NULL,
+	account_id BIGINT DEFAULT nextval('account_information_seq') PRIMARY KEY NOT NULL,
 	account_username VARCHAR(100),
 	account_password VARCHAR(100),
 	account_email VARCHAR(255),
@@ -13,13 +37,10 @@ CREATE TABLE ACCOUNT_INFORMATION
 	account_subscription_date DATE
 );
 
-DROP TABLE IF EXISTS BASE_WEAPON;  
-DROP SEQUENCE IF EXISTS base_weapon_seq;
-
 CREATE SEQUENCE base_weapon_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE BASE_WEAPON
 (
-    base_weapon_id BIGINT  DEFAULT NEXT VALUE FOR base_weapon_seq PRIMARY KEY NOT NULL,
+    base_weapon_id BIGINT  DEFAULT nextval('base_weapon_seq') PRIMARY KEY NOT NULL,
     base_weapon_name VARCHAR(100),
     base_stat_strength INT,
     base_stat_agility INT,
@@ -30,13 +51,10 @@ CREATE TABLE BASE_WEAPON
     base_stat_energy INT
 );
 
-DROP TABLE IF EXISTS CHARACTERS_STATS;  
-DROP SEQUENCE IF EXISTS characters_stats_seq;
-
 CREATE SEQUENCE characters_stats_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE CHARACTERS_STATS
 (
-    character_stats_id BIGINT  DEFAULT NEXT VALUE FOR characters_stats_seq PRIMARY KEY NOT NULL,
+    character_stats_id BIGINT  DEFAULT nextval('characters_stats_seq') PRIMARY KEY NOT NULL,
     character_strength INT,
     character_agility INT,
     character_intelligence INT,
@@ -47,13 +65,10 @@ CREATE TABLE CHARACTERS_STATS
     character_life FLOAT
 );
 
-DROP TABLE IF EXISTS CHARACTERS;  
-DROP SEQUENCE IF EXISTS characters_seq;
-
 CREATE SEQUENCE characters_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE CHARACTERS
 (
-    character_id BIGINT  DEFAULT NEXT VALUE FOR characters_seq PRIMARY KEY NOT NULL,
+    character_id BIGINT  DEFAULT nextval('characters_seq') PRIMARY KEY NOT NULL,
     account_id BIGINT NOT NULL ,
     base_weapon_id BIGINT DEFAULT NULL,
     character_stats_id BIGINT DEFAULT NULL,
@@ -64,22 +79,19 @@ CREATE TABLE CHARACTERS
     FOREIGN KEY(character_stats_id) REFERENCES CHARACTERS_STATS
 );
 
-
-
-DROP TABLE IF EXISTS SKILLS;  
-DROP SEQUENCE IF EXISTS skill_id_seq;
-
 CREATE SEQUENCE skill_id_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE SKILLS
 (
-    skill_id BIGINT  DEFAULT NEXT VALUE FOR skill_id_seq PRIMARY KEY NOT NULL,
+    skill_id BIGINT  DEFAULT nextval('skill_id_seq') PRIMARY KEY NOT NULL,
     base_weapon_id BIGINT,
-    skill_quality_code INT, -- check in NOMENCLATURE table
-    skill_rarity_code INT, -- check in NOMENCLATURE table
+    skill_quality_id INT, -- check in NOMENCLATURE table
+    skill_rarity_id INT, -- check in NOMENCLATURE table
     skill_quantity_available INT, -- 1 if unique
     skill_name VARCHAR(255),
     skill_effect VARCHAR(50), --damage flat/heal/dot/boost etc...
     skill_value_effect INT,
     skill_value_calcul VARCHAR(20), -- operator (add, multiply, percent,divide)
-    FOREIGN KEY(base_weapon_id) REFERENCES BASE_WEAPON
+    FOREIGN KEY(base_weapon_id) REFERENCES BASE_WEAPON,
+    FOREIGN KEY(skill_quality_id) REFERENCES NOMENCLATURE,
+    FOREIGN KEY(skill_rarity_id) REFERENCES NOMENCLATURE
 );
--- src/main/resources/db/migration/V1_02__adddata.sql
+++ src/main/resources/db/migration/V1_02__adddata.sql
@@ -1,3 +1,16 @@
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('QU1','ItemQuality','Basique');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('QU2','ItemQuality','Standard');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('QU3','ItemQuality','Avancé');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('QU4','ItemQuality','Elite');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('QU5','ItemQuality','Ultime');
+
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA1','ItemQuality','Commun');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA2','ItemQuality','Peu Commun');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA3','ItemQuality','Rare');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA4','ItemQuality','Epic');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA5','ItemQuality','Légendaire');
+INSERT INTO NOMENCLATURE(nomenclature_code,nomenclature_category,nomenclature_label) VALUES ('RA6','ItemQuality','Unique');
+
 INSERT INTO ACCOUNT_INFORMATION(account_username,account_password,account_email,account_age,account_birthday,account_subscription_date) VALUES ('test_username', 'test_password', 'test_mail',1,CURRENT_DATE,CURRENT_DATE);    
 
 INSERT INTO BASE_WEAPON(base_weapon_name,base_stat_strength,base_stat_agility,base_stat_intelligence,base_stat_wisdom,base_stat_stealth,base_stat_vitality,base_stat_energy) VALUES ('Bow',8,9,6,5,7,4,6); 
@@ -8,19 +21,20 @@ INSERT INTO BASE_WEAPON(base_weapon_name,base_stat_strength,base_stat_agility,ba
 INSERT INTO CHARACTERS_STATS(character_strength,character_agility,character_intelligence,character_wisdom,character_stealth,character_vitality,character_energy,character_life) 
 	VALUES (8,9,6,5,7,4,6,100.0); 
 INSERT INTO CHARACTERS(account_id,base_weapon_id,character_stats_id,character_name,character_title) 
-	VALUES (SELECT account_id FROM ACCOUNT_INFORMATION where account_username='test_username',
-			SELECT base_weapon_id from BASE_WEAPON where base_weapon_name='Bow',
-			SELECT character_stats_id from CHARACTERS_STATS where character_stats_id= 1 ,
+	VALUES ((SELECT account_id FROM ACCOUNT_INFORMATION where account_username='test_username'),
+			(SELECT base_weapon_id from BASE_WEAPON where base_weapon_name='Bow'),
+			(SELECT character_stats_id from CHARACTERS_STATS where character_stats_id= 1) ,
 			'Test_name1','Test_title'); 
 INSERT INTO CHARACTERS(account_id,character_name,character_title) 
-	VALUES (SELECT account_id FROM ACCOUNT_INFORMATION where account_username='test_username',
+	VALUES ((SELECT account_id FROM ACCOUNT_INFORMATION where account_username='test_username'),
 			'Test_name1','Test_title'); 
 
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (1,1,1,10,'Fire Arrow','damage flat',10,'substraction');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (1,1,1,10,'Celerity','boost agility',10,'add');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (2,1,1,10,'Slicer','damage flat',10,'substraction');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (2,1,1,10,'Bleed Hit','dot10sec',2,'substraction');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (3,1,1,10,'Fire Ball','damage flat',15,'substraction');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (3,1,1,10,'Ica Calamity','damage flat',15,'percent');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (4,1,1,10,'Holy Light','heal',10,'add');
-INSERT INTO SKILLS(base_weapon_id,skill_quality_code,skill_rarity_code,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (4,1,1,10,'White Light','debuff strenght',10,'percent');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (1,1,1,10,'Fire Arrow','damage flat',10,'substraction');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (1,1,1,10,'Celerity','boost agility',10,'add');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (2,1,1,10,'Slicer','damage flat',10,'substraction');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (2,1,1,10,'Bleed Hit','dot10sec',2,'substraction');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (3,1,1,10,'Fire Ball','damage flat',15,'substraction');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (3,1,1,10,'Ica Calamity','damage flat',15,'percent');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (4,1,1,10,'Holy Light','heal',10,'add');
+INSERT INTO SKILLS(base_weapon_id,skill_quality_id,skill_rarity_id,skill_quantity_available,skill_name,skill_effect,skill_value_effect,skill_value_calcul) VALUES (4,1,1,10,'White Light','debuff strenght',10,'percent');
+
